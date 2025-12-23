eda3e235ea3d29f344481ddff2f7d64d25619aed
VNCKey <villanewcode@gmail.com>
2025-12-22 23:21:27 -0500

    config compled

--- app-angular-18-vnc/src/app/app.config.ts
+++ app-angular-18-vnc/src/app/app.config.ts
@@ -1,9 +1,15 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withFetch()),
+    provideClientHydration()
+  ]
 };
