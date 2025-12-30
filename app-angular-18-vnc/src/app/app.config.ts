11dc606005a1d46b781a8e9868387ef72bb2da36
VNCKey <villanewcode@gmail.com>
2025-12-29 22:44:37 -0500

    conceptos de comparacion con la version 17

--- app-angular-18-vnc/src/app/app.config.ts
+++ app-angular-18-vnc/src/app/app.config.ts
@@ -1,15 +1,18 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from "@angular/core";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { routes } from "./app.routes";
+import { provideClientHydration } from "@angular/platform-browser";
+import { provideHttpClient, withFetch } from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withFetch()),
-    provideClientHydration()
-  ]
+    provideClientHydration(),
+  ],
 };
